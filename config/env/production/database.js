ad5086ed5f9e26dcad12296ed04b5f98d5d5160b
Adria <adriamartinez94@gmail.com>
2022-08-28 20:41:51 +0200

    Changed ip of DB

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
+      host: env("DATABASE_HOST", "0.0.0.0"),
       port: env.int("DATABASE_PORT", 7275),
       database: env("DATABASE_NAME", "railway"),
       user: env("DATABASE_USERNAME", "postgres"),
