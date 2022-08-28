563c5bdf560ca3b890028c88ab2bfbc8b6cf97f2
Adria <adriamartinez94@gmail.com>
2022-08-28 20:36:53 +0200

    added DB settings

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,10 +2,16 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USER"),
-      password: env("DATABASE_PASSWORD"),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 7275),
+      database: env("DATABASE_NAME", "railway"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "k5hWjMOFQcyoL8ABt0oH"),
+
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
+    debug: false,
   },
 });
